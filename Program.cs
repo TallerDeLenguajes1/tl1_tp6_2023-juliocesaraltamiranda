f811e393506b3ee8e393052780d3a3611b5069b1
AltamirandaJulio22 <102418766+AltamirandaJulio22@users.noreply.github.com>
2023-06-15 16:21:29 -0300

    finalizando tp

--- Program.cs
+++ Program.cs
@@ -45,14 +45,15 @@ if(ok) {
 //Ejercicio 2 
 string ?Stringmenu = "1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir\n5-Valor absoluto\n6-El cuadrado\n7-Raiz cuadrada\n8-El seno\n9-El coseno\n10-Parte entero de un tipo float";
 int opcionMenu = 0;
-int numA;
-int numB;
+int numA = 0;
+int numB = 0;
 int ejecutar = 1;
 int resultOperacion = 0;
 int resultMenu = 0;
 double radianes = 0;
 double resultOperacionDouble = 0;
 float numFloat = 0;
+string ?OperacionTexto = null;
 
 
 
@@ -80,7 +81,7 @@ while(ejecutar == 1) {
                 return;
             }
             resultOperacion = numA+numB;
-            
+            OperacionTexto = "La suma";
         break;
         case 2:
             Console.WriteLine("Ingresar el primer numero");
@@ -94,6 +95,7 @@ while(ejecutar == 1) {
             }
 
             resultOperacion = numA-numB;
+            OperacionTexto = "La resta";
             
         break;
         case 3:
@@ -108,7 +110,7 @@ while(ejecutar == 1) {
             }
 
             resultOperacion = numA*numB;
-            
+            OperacionTexto = "La multiplicacion";
         break;
         case 4:
             Console.WriteLine("Ingresar el primer numero");
@@ -122,7 +124,7 @@ while(ejecutar == 1) {
             }
             
             resultOperacion = numA/numB;
-            
+            OperacionTexto = "La division";
         break;       
         case 5:
             Console.WriteLine("Ingresar un numero");
@@ -137,7 +139,7 @@ while(ejecutar == 1) {
             if(numA<0) {
                 resultOperacion = -1*numA;
             }
-            
+            OperacionTexto = null;
         break;
         case 6:
             Console.WriteLine("Ingresar un numero");
@@ -149,7 +151,7 @@ while(ejecutar == 1) {
             }
 
             resultOperacion = numA*numA;
-
+            OperacionTexto = null;
         break;
         case 7:
             Console.WriteLine("Ingresar un numero");
@@ -161,6 +163,7 @@ while(ejecutar == 1) {
             }
 
             resultOperacionDouble = Math.Sqrt(numA);
+            OperacionTexto = null;
         break;
         case 8:
             Console.WriteLine("Ingresar un grado");
@@ -173,7 +176,7 @@ while(ejecutar == 1) {
 
             radianes = numA*(Math.PI / 180);
             resultOperacionDouble = Math.Sin(radianes);
-
+            OperacionTexto = null;
         break;
         case 9:
             Console.WriteLine("Ingresar un grado");
@@ -186,7 +189,7 @@ while(ejecutar == 1) {
 
             radianes = numA*(Math.PI / 180);
             resultOperacionDouble = Math.Cos(radianes);
-
+            OperacionTexto = null;
         break;
         case 10:
             Console.WriteLine("Ingresar un numero flotante");
@@ -198,6 +201,7 @@ while(ejecutar == 1) {
             }
 
             resultOperacion = (int)Math.Floor(numFloat);
+            OperacionTexto = null;
         break;
         default:
             Console.WriteLine("Ingrese una opcion valida");
@@ -208,7 +212,12 @@ while(ejecutar == 1) {
     if(opcionMenu == 7 || opcionMenu == 8 || opcionMenu == 9) {
         Console.WriteLine("Resultado operacion: "+resultOperacionDouble);    
     } else {
-        Console.WriteLine("Resultado operacion: "+resultOperacion);    
+        if(OperacionTexto != null ) { //Adaptacion por Ejercicio 4
+            Console.WriteLine($"{OperacionTexto} de {numA} y de {numB} es igual a: "+resultOperacion);
+        } else {
+            Console.WriteLine("Resultado operacion: "+resultOperacion);    
+        }
+        
     }
     
     Console.WriteLine("Desea realizar otra operacion? si = 1, no = 0");
@@ -239,8 +248,8 @@ if(num1<num2) {
 
 if(num2<num1) {
     Console.WriteLine("El minimo numero es:" + num2);
-} 
-*/
+} */
+
 
 //Ejercicio 4
 Console.WriteLine("Ingrese una cadena");
@@ -253,7 +262,7 @@ if(cadena != null) {
     Console.WriteLine($"Longitud de texto: 0");
 }
 
-Console.WriteLine("Ingrese un segunda cadena");
+Console.WriteLine("Ingrese una segunda cadena");
 string ?cadena2 = Console.ReadLine();
 Console.WriteLine($"Cadenas concatenadas: {cadena} {cadena2}");
 
@@ -261,3 +270,48 @@ if(cadena2 != null) {//por ej ingresar: Programador Universitario
     var subcadena = cadena2.Substring(12);
     Console.WriteLine("Substring: "+subcadena);//Universitario
 }
+
+Console.WriteLine("Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por elemento en pantalla: ");
+
+if(cadena != null) {
+
+    foreach (char c in cadena) {
+        Console.WriteLine(c);
+    }
+    
+    Console.WriteLine("Ingrese una busqueda");
+    string ?textoBusqueda = Console.ReadLine();
+    //Buscando en cadena
+    int indice = cadena.IndexOf(textoBusqueda);
+    if(indice >=0) {
+        Console.WriteLine("La cadena se encontro a partir del indice : "+indice);
+    }
+
+    //convirtiendo a mayuscula
+    Console.WriteLine("Convertido a mayuscula: "+cadena.ToUpper());
+    //convirtiendo a minuscula
+    Console.WriteLine("Convertido a minuscula: "+cadena.ToLower());
+    //Ingresar cadena con caracteres para usar split
+    Console.WriteLine("Ingresar cadena con caracter para usar split");
+    cadena = Console.ReadLine();
+    //para que este codigo funcione necesitamos ingresar una cadena con el caracter ,
+    Console.WriteLine("recorremos el arreglo que devuelve la funcion split:");
+    foreach(string item in cadena.Split(",")){
+        Console.WriteLine(item);
+    }
+
+    Console.WriteLine("Ingrese una ecuacion, para este caso ingrese una cadena de ecuacion suma");//ej:34+2
+    string ?ecuacion = Console.ReadLine();
+    int indexOperador = ecuacion.IndexOf('+');
+    
+    int primerNum = int.TryParse(ecuacion.Substring(0, indexOperador), out primerNum) ? primerNum: 0;
+    int segundoNum = int.TryParse(ecuacion.Substring(indexOperador+1), out segundoNum) ? segundoNum: 0;
+    int resultado = primerNum+segundoNum;
+    Console.WriteLine("Resultado: "+ resultado);
+
+    
+
+    
+
+}
+
